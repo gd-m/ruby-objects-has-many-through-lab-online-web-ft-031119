de9ad2e410d10bba55dba4bc22d39a2a39331a14
Gitanjali Mule <gitanjalidmule@gmail.com>
2019-03-28 20:04:49 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,3 +1,25 @@
 class Patient
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    
+  end
+
+  def self.all 
+    @@all 
+  end
+
+  def appointments
+    array = []
+    Appointment.all.collect do |app|
+      if app.patient == self
+        array << app 
+      end
+      end
+  end
 
 end
